31d8c4430fb666ea55f27942ff27980ae8cacc78
PuszkaPotato <kolodziej.weronika@proton.me>
2025-08-18 16:35:33 +0200

    Add project files.

---
+++ DarkwaterRoomCodeDisplay/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TMPro;
+
+namespace DarkwaterRoomCodeDisplay
+{
+    [BepInPlugin("eu.puszkapotato.darkwater.roomcodedisplay", "Darkwater Display Room Code", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            var harmony = new Harmony("eu.puszkapotato.darkwater.roomcodedisplay");
+            harmony.PatchAll();
+
+            Log.LogInfo("Darkwater Display Room Code mod has been loaded!");
+        }
+    }
+
+    [HarmonyPatch]
+    public class RoomCodeDisplayPatch
+    {
+        [HarmonyPatch(typeof(UI_SettingsMenu), "Update")]
+        [HarmonyPostfix]
+        public static void CheckPlayerListStatus(UI_SettingsMenu __instance)
+        {
+            // 1. Get the UI manager instance using the singleton pattern.
+            CMD_UI uiManager = CMD_UI.Instance;
+
+            // 2. Add safety checks to prevent errors if something hasn't loaded yet.
+            if (uiManager == null || uiManager.roomCodeDisplay == null || __instance.playerListPanel == null)
+            {
+                return;
+            }
+
+            // 3. Get a reference to the text object we want to control.
+            TextMeshProUGUI roomCodeDisplay = uiManager.roomCodeDisplay;
+
+            // 4. This is your core logic: check the state of the player list and
+            //    set the visibility of the room code to match.
+            roomCodeDisplay.gameObject.SetActive(__instance.playerListPanel.activeInHierarchy);
+        }
+    }
+}
---
+++ DarkwaterRoomCodeDisplay/README.md
@@ -0,0 +1,20 @@
+# Darkwater Display Room Code
+
+A BepInEx mod for Darkwater that displays the room code on the UI when the player list is open. This helps players easily share and join rooms.
+
+## Features
+- Shows the room code when the player list is visible.
+- Automatically hides the code when the player list is closed.
+
+## Installation
+1. Install [BepInEx](https://github.com/BepInEx/BepInEx) for Darkwater.
+2. Download this mod's DLL from the releases.
+3. Place the DLL in your `BepInEx/plugins` folder.
+
+## Usage
+Open the player list in-game to see the room code displayed. Close the list to hide the code.
+
+## Support
+For issues or suggestions, open an issue on [GitHub](https://github.com/PuszkaPotato/Darkwater-Dispaly-Room-Code).
+
+---
